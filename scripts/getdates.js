732af37ff9240feb75b2f4f9109df7bb8b51d5e6
pedromoo <moo23001@byui.edu>
2023-09-15 09:04:06 -0600

    Second draft

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,4 @@
 let oLastModif = new Date(document.lastModified);
+document.getElementById('year').innerHTML = oLastModif.getFullYear();
 let month = oLastModif.getMonth()+1;
 document.getElementById('lastModified').innerHTML = "Last modified on " + month + "/" + oLastModif.getDate() + "/" + oLastModif.getFullYear();
