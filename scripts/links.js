72643235b5aaf32619a1c0e50043e949c4da0f08
pedromoo <moo23001@byui.edu>
2023-11-08 17:24:01 -0600

    corrected linksURL

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
 const baseURL = "https://moo23001.github.io/wdd230/";
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const linksURL = "https://moo23001.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
